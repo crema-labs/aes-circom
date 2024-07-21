45d56b156ecbc050841634225052992f81c41eae
ayman <aymanshaik1015@gmail.com>
2024-07-21 09:31:35 +0530

    add subbytes template

---
+++ circuits/subbytes.circom
@@ -0,0 +1,6 @@
+pragma circom 2.0.0;
+
+template SubBytes(n) {
+    signal input key[n];
+    log(key);
+}
--- tests/subbytes.test.ts
+++ tests/subbytes.test.ts
@@ -1,39 +1,6 @@
 import { WitnessTester } from "circomkit";
 import { circomkit } from "./common";
 
-const fixtures = [
-  {
-    ideal: "abc.",
-    actual: "abcd",
-    expect: 1, //true
-  },
-  {
-    ideal: "abcd",
-    actual: "abcd",
-    expect: 1, //true
-  },
-  {
-    ideal: "a..d",
-    actual: "abcd",
-    expect: 1, //true
-  },
-  {
-    ideal: "....",
-    actual: "abcd",
-    expect: 1, //true
-  },
-  {
-    ideal: "abce",
-    actual: "abcd",
-    expect: 0, //false
-  },
-  {
-    ideal: "...e",
-    actual: "abcd",
-    expect: 0, //false
-  },
-];
-
 describe("SubBytes", () => {
   const f = (str: string) => Array.from(Buffer.from(str, "utf-8"));
   const length = 128;
@@ -50,7 +17,8 @@ describe("SubBytes", () => {
     });
 
     it(async () => {
-      circuit.compute({ key: f("abcd") }, []);
+      //   circuit.compute({ key: f("abcd") }, []);
+      await circuit.expectPass({ key: f("abcd") });
     });
   });
 });
